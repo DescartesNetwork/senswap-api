de7919e48923bc9a73c4648cc8d068add4fa1049
tphanson <tphanson@kambria.io>
2021-03-27 14:43:20 +0700

    uifix

--- configs/sol.config.js
+++ configs/sol.config.js
@@ -15,8 +15,8 @@ configs.development = {
   },
   node: 'https://devnet.solana.com',
   spltAddress: 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA',
-  mints: ['8jk4eJymMfNZV9mkRNxJEt2VJ3pRvdJvD5FE94GXGBPM', '27hdcZv7RtuMp75vupThR3T4KLsL61t476eosMdoec4c'],
-  accounts: ['4mBj4rqzwGZNJSwQT6ErjNhpJdT2DjYm7ShpepvXP9W8', '8UMCo415r51DBrjeTXCkmazwrgxTn6yvupvh3F1SvuQz'],
+  mints: ['8jk4eJymMfNZV9mkRNxJEt2VJ3pRvdJvD5FE94GXGBPM', '27hdcZv7RtuMp75vupThR3T4KLsL61t476eosMdoec4c', '5YwUkPdXLoujGkZuo9B4LsLKj3hdkDcfP4derpspifSJ'],
+  accounts: ['4mBj4rqzwGZNJSwQT6ErjNhpJdT2DjYm7ShpepvXP9W8', '8UMCo415r51DBrjeTXCkmazwrgxTn6yvupvh3F1SvuQz', '5UaNrW6GvGLzGBxwVp5Re2UY395n3nPt56skK2KLeheA'],
   airdropAmount: 100000000000n, // 100 tokens
   lamports: 0.1 * LAMPORTS_PER_SOL, // 0.01 SOL
 };
@@ -31,8 +31,8 @@ configs.staging = {
   },
   node: 'https://devnet.solana.com',
   spltAddress: 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA',
-  mints: ['8jk4eJymMfNZV9mkRNxJEt2VJ3pRvdJvD5FE94GXGBPM', '27hdcZv7RtuMp75vupThR3T4KLsL61t476eosMdoec4c'],
-  accounts: ['9W3JQdp1pUerAHtXUgVDmYrAtM8oSB7s5a1VQdBL6ifn', '3e7nCzPJiqwFf7opkvoHc9cdyzdrwmzTanStDJSKZkjn'],
+  mints: ['8jk4eJymMfNZV9mkRNxJEt2VJ3pRvdJvD5FE94GXGBPM', '27hdcZv7RtuMp75vupThR3T4KLsL61t476eosMdoec4c', '5YwUkPdXLoujGkZuo9B4LsLKj3hdkDcfP4derpspifSJ'],
+  accounts: ['4mBj4rqzwGZNJSwQT6ErjNhpJdT2DjYm7ShpepvXP9W8', '8UMCo415r51DBrjeTXCkmazwrgxTn6yvupvh3F1SvuQz', '5UaNrW6GvGLzGBxwVp5Re2UY395n3nPt56skK2KLeheA'],
   airdropAmount: 100000000000n, // 100 tokens
   lamports: 0.1 * LAMPORTS_PER_SOL, // 0.01 SOL
 };
--- db/pool.schema.js
+++ db/pool.schema.js
@@ -7,7 +7,6 @@ const Pool = new mongoose.Schema({
   address: { type: String, required: true, unique: true },
   network: { type: String, required: true },
   mint: { type: String, required: true },
-  author: { type: String, required: true },
   verified: { type: Boolean, default: false },
 }, { timestamps: true });
 
