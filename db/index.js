fe45f2bd20f9b6e94c3cb2b870512d5b2433ba48
tuphan-dn <71456318+tuphan-dn@users.noreply.github.com>
2021-07-18 18:04:13 +0700

    Update index.js

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const IP = require('./ip.schema');
 const Pool = require('./pool.schema');
 const User = require('./user.schema');
 const Mint = require('./mint.schema');
-const StakePool = require('./stake-pool.schema');
+const StakePool = require('./stakePool.schema');
 
 /**
  * Module exports
